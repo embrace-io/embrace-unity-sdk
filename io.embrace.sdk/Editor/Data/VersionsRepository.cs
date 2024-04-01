136018670d712b6a9b8888dcc6cedd7be6fd6011
alyssa-embrace <alyssa.syharath@embrace.io>
2024-04-01 16:16:56 -0400

    Update Version Repo to Android 6.5.0

--- io.embrace.sdk/Editor/Data/VersionsRepository.cs
+++ io.embrace.sdk/Editor/Data/VersionsRepository.cs
@@ -2,7 +2,7 @@ namespace EmbraceSDK.EditorView
 {
     public class VersionsRepository
     {
-        public const string ANDROID_SDK_VERSION = "6.3.2";
+        public const string ANDROID_SDK_VERSION = "6.5.0";
         public const string BUGSHAKE_SDK_VERSION = "0.9.0";
     }
 }
