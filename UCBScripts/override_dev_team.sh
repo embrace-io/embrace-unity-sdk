260aa613691636c98084993ee36359ebcad9d3d8
Alyssa Syharath <alyssasyharath@Alyssas-MacBook-Pro-2.local>
2025-04-23 21:40:10 -0400

    Modifying script to preserve unity YAML expected directives

--- UCBScripts/override_dev_team.sh
+++ UCBScripts/override_dev_team.sh
@@ -4,6 +4,11 @@ project_settings=($(find . -name ProjectSettings.asset))
 
 for file in "${project_settings[@]}"; do
     if grep -q "appleDeveloperTeamID:" "$file"; then
-        yq eval ".PlayerSettings.appleDeveloperTeamID = \"$APPLE_TEAM_ID\"" -i "$file"
+        header_lines=$(( $(awk '/PlayerSettings:/ {print NR; exit}' "$file") - 1 ))
+        head -n "$header_lines" "$file" > temp_header
+        tail -n +$((header_lines + 1)) "$file" > temp_body
+        yq eval ".PlayerSettings.appleDeveloperTeamID = \"$APPLE_TEAM_ID\"" -i "temp_body"
+        cat temp_header temp_body > "$file"
+        rm temp_header temp_body
     fi
 done
