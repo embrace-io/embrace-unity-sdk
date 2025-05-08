264a0411e3e022eabee1db64e8d7bf61cfc64502
Alyssa Syharath <alyssasyharath@Alyssas-MacBook-Pro-2.local>
2025-05-08 14:33:20 -0400

    Minimizing API surface for Editor scripts with internal

--- io.embrace.sdk/Editor/AssemblyInfo.cs
+++ io.embrace.sdk/Editor/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Embrace.EditTests")]
+[assembly: InternalsVisibleTo("Embrace.SDK.Editor.Weaver")]
+[assembly: InternalsVisibleTo("Embrace.Internal.SmokeTests")]
+[assembly: InternalsVisibleTo("Embrace.Internal.SmokeTests.Editor")]
+[assembly: InternalsVisibleTo("Embrace.Tools")]
