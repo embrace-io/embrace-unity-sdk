4599a2ed360cb125daaf1a54a769dbeba3c0f48a
alyssa-embrace <alyssa.syharath@embrace.io>
2025-07-02 12:41:24 -0400

    Embrace Config start

--- io.embrace.sdk/Editor/Data/EmbraceConfig.cs
+++ io.embrace.sdk/Editor/Data/EmbraceConfig.cs
@@ -1,4 +1,19 @@
 namespace EmbraceSDK.EditorView
 {
+	public abstract class EmbraceAppConfig
+	{
+		public abstract string AppIdKey { get; }
+		public abstract string SymbolUploadTokenKey { get; }
 
+		public string AppId
+		{
+			get => EmbraceProjectSettings.Project.GetValue<string>(AppIdKey, string.Empty);
+			set => EmbraceProjectSettings.Project.SetValue<string>(AppIdKey, value);
+		}
+
+		public string SymbolUploadApiToken {
+			get => EmbraceProjectSettings.Project.GetValue<string>(SymbolUploadTokenKey, string.Empty);
+			set => EmbraceProjectSettings.Project.SetValue<string>(SymbolUploadTokenKey, value);
+		}
+	}
 }
