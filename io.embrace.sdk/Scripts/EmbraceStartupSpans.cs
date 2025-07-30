a48336fa4b55081429c3edb2f0efe53c8ecbca38
Benjamin Bennett <benjabenn@gmail.com>
2025-07-30 16:32:20 -0700

    Updated child spans
    Because we can't start spans before the SDK is setup I had to create a collection to store them and then just record them once we run EndAppStartup

--- io.embrace.sdk/Scripts/EmbraceStartupSpans.cs
+++ io.embrace.sdk/Scripts/EmbraceStartupSpans.cs
@@ -12,8 +12,14 @@ namespace EmbraceSDK
     /// </summary>
     public static class EmbraceStartupSpans
     {
-        public static string ParentSpanId { get; private set; }
+        private class ChildSpan
+        {
+            public Dictionary<string, string> Attributes;
+            public long StartTime;
+            public long EndTime;
+        }
 
+        private static readonly Dictionary<string, ChildSpan> _childSpans = new();
         private static DateTimeOffset _appStartTime;
         private static DateTimeOffset _firstSceneLoadedTime;
         private static DateTimeOffset _embraceSDKStartTime;
@@ -23,7 +29,6 @@ namespace EmbraceSDK
         public static void StartApplication()
         {
             _appStartTime = DateTimeOffset.UtcNow;
-            ParentSpanId = Embrace.Instance.StartSpan("emb-app-startup", _appStartTime.ToUnixTimeMilliseconds());
         }
         
         #if EMBRACE_STARTUP_SPANS_FIRST_SCENE_LOADED
@@ -44,15 +49,27 @@ namespace EmbraceSDK
             _embraceSDKEndTime = DateTimeOffset.UtcNow;
         }
         
-        public static string StartChildSpan(string spanName)
+        public static void StartChildSpan(string spanName)
         {
             if (string.IsNullOrEmpty(spanName))
             {
                 Debug.LogWarning("EmbraceStartupSpans: Span name must not be null or empty.");
-                return null;
+                return;
             }
 
-            return Embrace.Instance.StartSpan(spanName, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(), parentSpanId: ParentSpanId);
+            if (_childSpans.ContainsKey(spanName))
+            {
+                Debug.LogWarning($"EmbraceStartupSpans: Span '{spanName}' already exists. It will be overwritten.");
+                _childSpans.Remove(spanName);
+            }
+            
+            long startTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            _childSpans[spanName] = new ChildSpan
+            {
+                Attributes = new Dictionary<string, string>(),
+                StartTime = startTime,
+                EndTime = 0
+            };
         }
         
         public static void StopChildSpan(string spanId, Dictionary<string, string> attributes = null)
@@ -62,38 +79,49 @@ namespace EmbraceSDK
                 Debug.LogWarning("EmbraceStartupSpans: Span ID must not be null or empty.");
                 return;
             }
-            
-            if (attributes != null)
-            {
-                foreach (var kvp in attributes)
-                {
-                    Embrace.Instance.AddSpanAttribute(spanId, kvp.Key, kvp.Value);
-                }
-            }
 
-            Embrace.Instance.StopSpan(spanId, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+            if (_childSpans.TryGetValue(spanId, out var span) == false)
+            {
+                Debug.LogWarning($"EmbraceStartupSpans: Span '{spanId}' does not exist. Cannot stop span.");
+                return;
+            }
+            
+            long endTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            span.EndTime = endTime;
+            span.Attributes = attributes;
         }
 
         public static void EndAppStartup(Dictionary<string, string> attributes = null)
         {
+            string parentSpanId = Embrace.Instance.StartSpan("emb-app-startup", _appStartTime.ToUnixTimeMilliseconds());
             
 #if EMBRACE_STARTUP_SPANS_FIRST_SCENE_LOADED
-            Embrace.Instance.RecordCompletedSpan("emb-app-loaded", _appStartTime.ToUnixTimeMilliseconds(), _firstSceneLoadedTime.ToUnixTimeMilliseconds(), parentSpanId: ParentSpanId);
+            Embrace.Instance.RecordCompletedSpan("emb-app-loaded", _appStartTime.ToUnixTimeMilliseconds(), _firstSceneLoadedTime.ToUnixTimeMilliseconds(), parentSpanId: parentSpanId);
 #endif
 #if EMBRACE_STARTUP_SPANS_LOADING_COMPLETE
-            Embrace.Instance.RecordCompletedSpan("emb-app-time-to-interactive", _firstSceneLoadedTime.ToUnixTimeMilliseconds(), DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(), parentSpanId: ParentSpanId);
+            Embrace.Instance.RecordCompletedSpan("emb-app-time-to-interactive", _firstSceneLoadedTime.ToUnixTimeMilliseconds(), DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(), parentSpanId: parentSpanId);
 #endif
-            Embrace.Instance.RecordCompletedSpan("emb-embrace-init", _embraceSDKStartTime.ToUnixTimeMilliseconds(), _embraceSDKEndTime.ToUnixTimeMilliseconds(), parentSpanId: ParentSpanId);
+            Embrace.Instance.RecordCompletedSpan("emb-embrace-init", _embraceSDKStartTime.ToUnixTimeMilliseconds(), _embraceSDKEndTime.ToUnixTimeMilliseconds(), parentSpanId: parentSpanId);
 
+            // add any additional attributes to the parent span
             if (attributes != null)
             {
                 foreach (var kvp in attributes)
                 {
-                    Embrace.Instance.AddSpanAttribute(ParentSpanId, kvp.Key, kvp.Value);
+                    Embrace.Instance.AddSpanAttribute(parentSpanId, kvp.Key, kvp.Value);
                 }
             }
+
+            // record all child spans
+            foreach ((string spanName, var childSpan) in _childSpans)
+            {
+                Embrace.Instance.RecordCompletedSpan(spanName, childSpan.StartTime, childSpan.EndTime, parentSpanId: parentSpanId);
+            }
             
-            Embrace.Instance.StopSpan(ParentSpanId, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+            _childSpans.Clear();
+            
+            // finally, stop the parent span
+            Embrace.Instance.StopSpan(parentSpanId, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
         }
     }
     #endif
