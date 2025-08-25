8cc148fdb25fb3cb41e8c3d74786949b5a07d857
Benjamin Bennett <benjabenn@gmail.com>
2025-08-25 09:18:17 -0700

    Update scene loading spans
    Added scene allow list, updated settings/spans

--- io.embrace.sdk/Editor/Editor Windows/Settings/EmbraceSpansManager.cs
+++ io.embrace.sdk/Editor/Editor Windows/Settings/EmbraceSpansManager.cs
@@ -15,6 +15,7 @@ namespace EmbraceSDK.EditorView
         public const string EMBRACE_STARTUP_SPANS_DEFINE = "EMBRACE_STARTUP_SPANS";
         public const string EMBRACE_STARTUP_SPANS_FIRST_SCENE_LOADED_DEFINE = "EMBRACE_STARTUP_SPANS_FIRST_SCENE_LOADED";
         public const string EMBRACE_STARTUP_SPANS_LOADING_COMPLETE_DEFINE = "EMBRACE_STARTUP_SPANS_LOADING_COMPLETE";
+        public const string EMBRACE_SCENE_LOAD_SPANS_DEFINE = "EMBRACE_SCENE_LOAD_SPANS";
         
         [Serializable]
         public struct SpanFlags
@@ -27,19 +28,33 @@ namespace EmbraceSDK.EditorView
         private SpanFlags _spanFlags;
         
         [Tooltip(EmbraceTooltips.StartupSpanCapture)]
-        private bool _enabled;
+        private bool _startupSpansCaptureEnabled;
+        private bool _sceneLoadSpansEnabled;
         
         public override void OnGUI()
         {
             DrawStartupSpans();
             EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            DrawSceneLoadSpans();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            
+            if(GUILayout.Button("Apply Settings"))
+            {
+                _scriptingDefineUtil.ToggleSymbol(EMBRACE_STARTUP_SPANS_DEFINE, _startupSpansCaptureEnabled);
+                _scriptingDefineUtil.ToggleSymbol(EMBRACE_STARTUP_SPANS_FIRST_SCENE_LOADED_DEFINE, _spanFlags.RecordFirstSceneLoaded && _startupSpansCaptureEnabled);
+                _scriptingDefineUtil.ToggleSymbol(EMBRACE_STARTUP_SPANS_LOADING_COMPLETE_DEFINE, _spanFlags.RecordLoadingComplete && _startupSpansCaptureEnabled);
+                _scriptingDefineUtil.ToggleSymbol(EMBRACE_SCENE_LOAD_SPANS_DEFINE, _sceneLoadSpansEnabled);
+                _scriptingDefineUtil.ApplyModifiedProperties();
+            }
         }
 
         private void DrawStartupSpans()
         {
             GUILayout.Label("Startup Spans", EditorStyles.boldLabel);
-            _enabled = EditorGUILayout.Toggle(new GUIContent("emb-app-time-to-interact", EmbraceTooltips.StartupSpanCapture), _enabled);
-            EditorGUI.BeginDisabledGroup(!_enabled);
+            _startupSpansCaptureEnabled = EditorGUILayout.Toggle(new GUIContent("emb-app-time-to-interact", EmbraceTooltips.StartupSpanCapture), _startupSpansCaptureEnabled);
+            EditorGUI.BeginDisabledGroup(!_startupSpansCaptureEnabled);
             SpanFlags flags = new SpanFlags
             {
                 RecordFirstSceneLoaded = EditorGUILayout.Toggle(new GUIContent("emb-app-loaded", EmbraceTooltips.StartupSpanFirstSceneLoaded), _spanFlags.RecordFirstSceneLoaded),
@@ -49,21 +64,21 @@ namespace EmbraceSDK.EditorView
             EditorGUILayout.Space();
             _spanFlags = flags;
             EditorGUILayout.HelpBox("When your app is finished loading and your user is ready to start interacting with your app you will need to call Embrace.Instance.EndAppStartup() in your code.", MessageType.Info);
-            
-            if(GUILayout.Button("Apply Settings"))
-            {
-                _scriptingDefineUtil.ToggleSymbol(EMBRACE_STARTUP_SPANS_DEFINE, _enabled);
-                _scriptingDefineUtil.ToggleSymbol(EMBRACE_STARTUP_SPANS_FIRST_SCENE_LOADED_DEFINE, _spanFlags.RecordFirstSceneLoaded && _enabled);
-                _scriptingDefineUtil.ToggleSymbol(EMBRACE_STARTUP_SPANS_LOADING_COMPLETE_DEFINE, _spanFlags.RecordLoadingComplete && _enabled);
-                _scriptingDefineUtil.ApplyModifiedProperties();
-            }
+        }
+        
+        private void DrawSceneLoadSpans()
+        {
+            GUILayout.Label("Scene Load Spans", EditorStyles.boldLabel);
+            _sceneLoadSpansEnabled = EditorGUILayout.Toggle(new GUIContent("Enable Scene Load Spans", EmbraceTooltips.SceneLoadSpans), _sceneLoadSpansEnabled);
+            EditorGUILayout.HelpBox("The Embrace SDK can automatically measure scene load times for you. This is done by overriding Unity's SceneManagerAPI. If you are already using a custom SceneManagerAPI override, this will not work or may conflict.", MessageType.Info);
         }
 
         public override void Initialize(MainSettingsEditor _)
         {
             base.Initialize(mainSettingsEditor);
             _scriptingDefineUtil = new ScriptingDefineUtil();
-            _enabled = _scriptingDefineUtil.CheckIfSettingIsEnabled(EMBRACE_STARTUP_SPANS_DEFINE);
+            _startupSpansCaptureEnabled = _scriptingDefineUtil.CheckIfSettingIsEnabled(EMBRACE_STARTUP_SPANS_DEFINE);
+            _sceneLoadSpansEnabled = _scriptingDefineUtil.CheckIfSettingIsEnabled(EMBRACE_SCENE_LOAD_SPANS_DEFINE);
             _spanFlags = new SpanFlags
             {
                 RecordFirstSceneLoaded = _scriptingDefineUtil.CheckIfSettingIsEnabled(EMBRACE_STARTUP_SPANS_FIRST_SCENE_LOADED_DEFINE),
