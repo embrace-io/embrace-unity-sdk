88449a5c2a7787706f9d237fabba9cc963c67e7d
Benjamin Bennett <benjabenn@gmail.com>
2025-08-25 09:18:17 -0700

    Update scene loading spans
    Added scene allow list, updated settings/spans

--- io.embrace.sdk/Scripts/Utilities/SceneLoadMeasurer.cs
+++ io.embrace.sdk/Scripts/Utilities/SceneLoadMeasurer.cs
@@ -1,18 +1,40 @@
+#if EMBRACE_SCENE_LOAD_SPANS
+
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace EmbraceSDK.Utilities
 {
+    /// <summary>
+    /// Measures scene load times using Unity's SceneManagerAPI override.
+    /// If you are already using a custom SceneManagerAPI override, this will not work or may conflict.
+    /// </summary>
     public static class SceneLoadMeasurer
     {
+        private static List<string> _sceneAllowList;
         private static string _currentSceneLoadSpanId;
+        
+        /// <summary>
+        /// Call this function with a list of scenes you want to measure. If this is not called, all scenes will be measured.
+        /// </summary>
+        /// <param name="sceneAllowList"></param>
+        public static void SetSceneAllowList(List<string> sceneAllowList)
+        {
+            _sceneAllowList = sceneAllowList;
+        }
         
         private static void OnSceneLoadStarted(string sceneName)
         {
             if (Embrace.Instance.IsStarted == false)
             {
                 Debug.LogWarning("Unable to start scene load span because Embrace is not started.");
+                return;
+            }
+
+            if (_sceneAllowList is { Count: > 0 } && _sceneAllowList.Contains(sceneName) == false)
+            {
                 return;
             }
 
@@ -37,3 +59,4 @@ namespace EmbraceSDK.Utilities
         }
     }
 }
+#endif
