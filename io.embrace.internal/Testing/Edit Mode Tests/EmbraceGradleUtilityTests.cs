9f1648f43391a5ad169df5849a035c7607d60779
Benjamin Bennett <benjabenn@gmail.com>
2026-06-03 09:59:26 -0700

    Add missing System.Text.RegularExpressions using to EmbraceGradleUtilityTests

--- io.embrace.internal/Testing/Edit Mode Tests/EmbraceGradleUtilityTests.cs
+++ io.embrace.internal/Testing/Edit Mode Tests/EmbraceGradleUtilityTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using EmbraceSDK.EditorView;
 using NUnit.Framework;
 using UnityEditor.Build;
